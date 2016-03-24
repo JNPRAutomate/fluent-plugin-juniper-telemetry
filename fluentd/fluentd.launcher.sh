0148834c01c2b053a059645b5ca42453792e5470
Damien <dgarros@gmail.com>
2016-03-23 18:19:55 -0700

    Update launcher to use local file

--- fluentd/fluentd.launcher.sh
+++ fluentd/fluentd.launcher.sh
@@ -2,4 +2,7 @@
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
 # fluentd -c /fluent/fluent.conf -vv
-fluentd -c /fluent/fluent.conf >>/var/log/fluentd.log 2>&1
+fluentd -c /root/fluentd-plugin-juniper-telemetry/fluentd/fluent.conf \
+        -v \
+        -p /root/fluentd-plugin-juniper-telemetry/lib/fluent/plugin \
+        >> /var/log/fluentd.log 2>&1
