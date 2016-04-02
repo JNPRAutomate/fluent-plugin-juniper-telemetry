71a33e70f93734bbffaf0d7666739993fb9e1894
Damien <dgarros@gmail.com>
2016-04-02 11:31:30 -0700

    Update Analyticsd parser to use shared build_record function

--- lib/fluent/plugin/parser_juniper_analyticsd.rb
+++ lib/fluent/plugin/parser_juniper_analyticsd.rb
@@ -24,7 +24,6 @@ module Fluent
       def parse(text)
 
         payload = JSON.parse(text)
-        time = Engine.now
 
         ## Extract contextual info
         record_type = payload["record-type"]
@@ -45,64 +44,34 @@ module Fluent
           payload.delete("port")
 
           payload.each do |key, value|
-            record = {}
 
-            if output_format.to_s == 'flat'
-              full_name = "device.#{clean_up_name(device_name)}.interface.#{clean_up_name(port_name)}.type.#{key}"
+            # Save all info extracted on a list
+            sensor_data = []
+            sensor_data.push({ 'device' => device_name })
+            sensor_data.push({ 'interface' => port_name })
+            sensor_data.push({ 'type' => record_type + '.' + key })
+            sensor_data.push({ 'value' => value })
 
-              record[full_name.downcase]= value
-
-            elsif output_format.to_s == 'structured'
-              record['device'] = device_name
-              record['type'] = record_type + '.' + key
-              record['interface'] = port_name
-              record['value'] = value
-
-            elsif output_format.to_s == 'statsd'
-
-              full_name = "interface.#{clean_up_name(port_name)}.type.#{key}"
-              record[:statsd_type] = 'gauge'
-              record[:statsd_key] = full_name.downcase
-              record[:statsd_gauge] = value
-            else
-                $log.warn "Output_format '#{output_format.to_s}' not supported for #{record_type}"
-            end
-
+            record = build_record(output_format, sensor_data)
             yield json_time, record
           end
         elsif record_type == 'queue-stats'
 
-            ## Delete contextual info
-            payload.delete("record-type")
-            payload.delete("time")
-            payload.delete("router-id")
-            payload.delete("port")
+          ## Delete contextual info
+          payload.delete("record-type")
+          payload.delete("time")
+          payload.delete("router-id")
+          payload.delete("port")
 
-            payload.each do |key, value|
-            record = {}
+          payload.each do |key, value|
 
-            if output_format.to_s == 'flat'
+            sensor_data = []
+            sensor_data.push({ 'device' => device_name })
+            sensor_data.push({ 'interface' => port_name })
+            sensor_data.push({ 'type' => record_type + '.' + key })
+            sensor_data.push({ 'value' => value })
 
-              full_name = "device.#{clean_up_name(device_name)}.interface.#{clean_up_name(port_name)}.queue.#{key}"
-
-              record[full_name.downcase]= value
-
-            elsif output_format.to_s == 'structured'
-              record['device'] = device_name
-              record['type'] = record_type + '.' + key
-              record['interface'] = port_name
-              record['value'] = value
-
-            elsif output_format.to_s == 'statsd'
-              full_name = "interface.#{clean_up_name(port_name)}.queue.#{key}"
-              record[:statsd_type] = 'gauge'
-              record[:statsd_key] = full_name.downcase
-              record[:statsd_gauge] = value
-
-            else
-              $log.warn "Output_format '#{output_format.to_s}' not supported for #{record_type}"
-            end
-
+            record = build_record(output_format, sensor_data)
             yield json_time, record
           end
         else
