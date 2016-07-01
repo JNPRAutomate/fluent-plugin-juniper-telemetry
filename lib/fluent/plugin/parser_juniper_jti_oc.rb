34d57f8a67a33ddc7f0af9a95ebb4616d634a39a
Damien <dgarros@gmail.com>
2016-06-30 23:15:10 -0700

    Add support for OC/UDP

--- lib/fluent/plugin/parser_juniper_jti_oc.rb
+++ lib/fluent/plugin/parser_juniper_jti_oc.rb
@@ -1,6 +1,6 @@
 require 'juniper_telemetry_lib.rb'
 require 'protobuf'
-require 'resource.pb.rb'
+require 'open-config.pb.rb'
 
 module Fluent
   class TextParser
@@ -15,20 +15,43 @@ module Fluent
 
       def parse(text)
 
+        # $log.info  "juniper_jti_oc/ Starting Parse "
+        time = Engine.now
+
         ## Decode GBP packet
-        oc_msg =  Resource::Resource.decode(text)
+        oc_msg =  OpenConfigData.decode(text)
 
-        resource = ""
+        # $log.info  "Object #{oc_msg.inspect.to_s}"
 
-        $log.debug  " Message : " + oc_msg.inspect.to_s
-        #
-        # ## Extract device name & Timestamp
-        # device_name = jti_msg.system_id
-        # gpb_time = epoc_to_sec(jti_msg.timestamp)
+        device_id = oc_msg.system_id
+        component_id = oc_msg.component_id
+        gpb_time = epoc_to_sec(oc_msg.timestamp)
 
+        oc_msg.kv.each do |kv|
 
+          record = {}
 
+          # Add device and component id
+          record['device'] = device_id
+          record['component'] = component_id
 
+          # Extract type info
+          type = kv.key.match(/([a-zA-Z\-\_]*)$/)
+          record['type'] = type
+
+          # Extract Value
+          record['value'] = kv.int_value
+
+          # Try to extract attribute information
+          # Check how it will looks with multiple attribute per string
+          attribute = kv.key.match(/\/([^\/]*)\[([A-Za-z0-9\-]*)\=([^\[]*)\]/)
+          if attribute
+            if attribute[2] == "name"
+              record[attribute[1]] = attribute[3]
+            end
+          end
+          yield gpb_time, record
+        end
       end
     end
   end
